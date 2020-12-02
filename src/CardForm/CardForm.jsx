d5cfba0741c22eaf2813051aec6ede141e1e7ef8
Shaked Zrihen <szrihen@cisco.com>
2020-12-02 20:46:31 +0200

    LOL

--- src/CardForm/CardForm.jsx
+++ src/CardForm/CardForm.jsx
@@ -21,6 +21,7 @@ const CardForm = (props) => {
   const [description, setDescription] = useState("");
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+  const [currentUser] = useState(localStorage.getItem("currentUser"));
 
   const [emailError, setEmailErrorMsg] = useState(VALIDATION_PASSED);
   const isEmailError = emailError !== VALIDATION_PASSED;
