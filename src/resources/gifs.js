2cf6dc23f4283657a84fca5f2b89e04465c5b3d5
Noa Finkel <nofinkel@cisco.com>
2020-12-02 20:16:25 +0200

    bla

--- src/resources/gifs.js
+++ src/resources/gifs.js
@@ -2,10 +2,10 @@ export default [
   "https://robotunion.eu/wp-content/uploads/2019/06/giphy-2.gif",
   "https://i.pinimg.com/originals/3e/87/b5/3e87b5e21e8b25f967c545fedcdf9230.gif",
   "https://c.tenor.com/lVlj3Z219BMAAAAj/sound-classical-music.gif",
-  "https://media1.giphy.com/media/jUgXFVAyaXbDNg6hcd/giphy.gif",
+  "https://media3.giphy.com/media/jUgXFVAyaXbDNg6hcd/giphy.gif",
   "https://i.pinimg.com/originals/59/a5/f0/59a5f025e75fe29104bd9597f5f7b0f1.gif",
   "https://i.pinimg.com/originals/e7/ab/1e/e7ab1ede1eea99b1de9a8f6eaf0582a9.gif",
-  "https://media1.tenor.com/images/c7059cf0c1ebc7c3ec3562028d141f9d/tenor.gif",
+  "https://media.giphy.com/media/fjxeswpTKg3Uy2INQx/giphy.gif",
   "https://media3.giphy.com/media/ZFo9Jf12rYxWwr1mFd/source.gif",
 ];
 
