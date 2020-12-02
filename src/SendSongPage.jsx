df2c62a32e90cd7ab6d4437f7b56c745a46ee067
Shaked Zrihen <szrihen@cisco.com>
2020-12-02 11:24:08 +0200

    Fix css style problem

--- src/SendSongPage.jsx
+++ src/SendSongPage.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
 import { Container } from "@material-ui/core";
 import CardForm from "./CardForm/CardForm";
 import useStyles from "./style";
 import clsx from "clsx";
-import "./style.scss";
+import "./SendSongPage.scss";
 
 const SendSongPage = () => {
   const classes = useStyles();
 
+  useEffect(() => {
+    document.body.id = "sendSongBody";
+
+    return () => {
+      document.body.id = "";
+    };
+  }, []);
+
   return (
-    <div className="frame">
+    <div className="sendSongFrame">
       <Container
-        className={clsx(classes.container, "songHugContainer")}
+        className={clsx(classes.container, "sendSongHugContainer")}
         maxWidth="xl"
       >
         <Header />
--- src/SongHugPage.jsx
+++ src/SongHugPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container } from "@material-ui/core";
 import useStyles from "./style";
 import clsx from "clsx";
@@ -8,8 +8,16 @@ import SongHug from "./SongHug/SongHug";
 const SongHugPage = () => {
   const classes = useStyles();
 
+  useEffect(() => {
+    document.body.id = "songHugBody";
+
+    return () => {
+      document.body.id = "";
+    };
+  }, []);
+
   return (
-    <div className="frame">
+    <div className="songHugFrame">
       <Container
         className={clsx(classes.container, "songHugContainer")}
         maxWidth="xl"
