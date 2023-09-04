54c70f97dbc96f3d11552cd59d5e2b0609282555
shaked zohar <shaked.zrihen@gmail.com>
2023-09-04 21:32:04 +0300

    Update shaked.spec.js

--- shaked.spec.js
+++ shaked.spec.js
@@ -2,6 +2,15 @@ describe('Some tests', () => {
   it('Should test something' ,() => {
     console.log("hi");
   })
+   it('Should test something' ,() => {
+    console.log("hi");
+  })
+   it('Should test something' ,() => {
+    console.log("hi");
+  })
+   it('Should test something' ,() => {
+    console.log("hi");
+  })
 })
 
 
