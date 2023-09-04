ee8b075e14a28a8b726b9a70e12a0ce22e60f492
shaked zohar <shaked.zrihen@gmail.com>
2023-09-04 21:23:16 +0300

    Update shaked.spec.js

--- shaked.spec.js
+++ shaked.spec.js
@@ -2,65 +2,6 @@ describe('Some tests', () => {
   it('Should test something' ,() => {
     console.log("hi");
   })
-
-   it('Should test something else' ,() => {
-    console.log("hi!");
-  })
-    it('Should test something' ,() => {
-    console.log("hi");
-  })
-   it('Should test somethingush' ,() => {
-    console.log("hi");
-  })
-   it('Should test somethingushi' ,() => {
-    console.log("hi");
-  })
-  describe('Some tests', () => {
-  it('Should test something' ,() => {
-    console.log("hi");
-  })
-
-   it('Should test something else' ,() => {
-    console.log("hi!");
-  })
-    it('Should test something' ,() => {
-    console.log("hi");
-  })
-   it('Should test somethingush' ,() => {
-    console.log("hi");
-  })
-   it('Should test somethingushi' ,() => {
-    console.log("hi");
-  })
-    it('Should test something' ,() => {
-    console.log("hi");
-  })
-
-   it('Should test something else' ,() => {
-    console.log("hi!");
-  })
-    it('Should test something' ,() => {
-    console.log("hi");
-  })
-   it('Should test somethingush' ,() => {
-    console.log("hi");
-  })
-   it('Should test somethingushi' ,() => {
-    console.log("hi");
-  })
-    it('Should test something else' ,() => {
-    console.log("hi!");
-  })
-    it('Should test something else' ,() => {
-    console.log("hi!");
-  })
-     it('Should test something else' ,() => {
-    console.log("hi!");
-  })
-    it('Should test something else' ,() => {
-    console.log("hi!");
-  })
-})
 })
 
 
