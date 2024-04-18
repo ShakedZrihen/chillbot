e99953853ab0457838a6713ef9deab512ed2e12f
shaked zohar <shaked.zrihen@gmail.com>
2024-04-18 22:35:14 +0300

    Update index.js

--- .cm/plugins/filters/checklist/index.js
+++ .cm/plugins/filters/checklist/index.js
@@ -1,58 +1,45 @@
 const checklistFilter = (empty, branch, files, pr, repo) => { 
-	// Now that we have all the needed data here, we can use it to build our checklist
-	/*
-	console.log("Branch")
-	console.log(branch)
-	console.log("Files")
-	console.log(files)
-	console.log("PR")
-	console.log(pr)
-	console.log("Repo")
-	console.log(repo)
-	// */
-	/* 
-		This is an array of objects, each of which represents a check.
-		Not all *must* be true for the PR to be accepted, which is why
-		we don't just put them as automation conditions in the .cm file.
-	*/
-	const checks = [
-		{
-			title: "low-risk",
-			label: "The PR is a low-risk change",
-			condition: true
-		},
-		{
-			title: "has-jira",
-			label: "The PR has a Jira reference in the title",
-			condition: true
-		},
-		{
-			title: "updates-tests",
-			label: "The PR includes updates to tests",
-			condition: true
-		},
-		{
-			title: "includes-docs",
-			label: "The PR includes reference documentation",
-			condition: true
-		},
-		{
-			title: "includes-docs",
-			label: "The PR author is a first-time contributor",
-			condition: true
-		},
-		{
-			title: "requires-opsec",
-			label: "The PR requires OpSec approval",
-			condition: true
-		}
-	];
-
-	const comment = checks
-		.map(check => `- [${check.condition ? "x" : " "}] ${check.label}`)
-		.join("\n");
+    
+    console.log({empty, branch, files, pr, repo});
 	
-	return comment;
+    const checks = [
+        {
+            title: "low-risk",
+            label: "The PR is a low-risk change",
+            condition: true
+        },
+        {
+            title: "has-jira",
+            label: "The PR has a Jira reference in the title",
+            condition: true
+        },
+        {
+            title: "updates-tests",
+            label: "The PR includes updates to tests",
+            condition: true
+        },
+        {
+            title: "includes-docs",
+            label: "The PR includes reference documentation",
+            condition: true
+        },
+        {
+            title: "first-time-contrib",
+            label: "The PR author is a first-time contributor",
+            condition: true
+        },
+        {
+            title: "requires-opsec",
+            label: "The PR requires OpSec approval",
+            condition: true
+        }
+    ];
+
+    const comment = checks
+        .map(check => `- [${check.condition ? "x" : " "}] ${check.label}`)
+        .join("\n");
+    
+    return comment;
 };
 
-module.exports = checklistFilter
+module.exports = checklistFilter;
